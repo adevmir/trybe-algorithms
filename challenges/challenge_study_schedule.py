9265710674a809ea783e6df1fb634d093f5be025
adevmir <ademirvalonqar@gmail.com>
2023-03-22 17:13:16 -0300

    Corrigindo Linter + Alterando func study_schedule que tinha muita complexidade

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    try:
-        if target_time == None:
+    result = 0
+    for student in permanence_period:
+        if type(target_time) != int\
+            or type(student[0]) != int\
+                or type(student[1]) != int:
             return None
-        result = 0
-        for student in permanence_period:
-            if target_time in range(student[0], student[1] + 1):
-                result += 1
-        return result
-    except TypeError:
-        return None
+        if target_time in range(student[0], student[1] + 1):
+            result += 1
+    return result
