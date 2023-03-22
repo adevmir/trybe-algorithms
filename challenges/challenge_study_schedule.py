f8df8fa0eab2f7630b554c3580170da08d348db9
adevmir <ademirvalonqar@gmail.com>
2023-03-22 16:45:53 -0300

    [Requisito 01] - Recebe uma Tupla que representa o horario de entrada e saida de alunos de um sistema e um INT que representa o horario chave, em seguida avalia e retorna a quantidade de alunos logados no sistema naquele tempo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        if target_time == None:
+            return None
+        result = 0
+        for student in permanence_period:
+            if target_time in range(student[0], student[1] + 1):
+                result += 1
+        return result
+    except TypeError:
+        return None
