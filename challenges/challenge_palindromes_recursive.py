8b2cd5af5d351934388417d3ed50d40530ef37de
adevmir <ademirvalonqar@gmail.com>
2023-03-22 18:06:52 -0300

    Corrigindo Linter

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0 or word[low_index] != word[high_index]:
         return False
-    if len(word) == 1 or len(word) == 2 and word[low_index] == word[high_index]:
+    if len(word) == 1 or len(word) == 2\
+            and word[low_index] == word[high_index]:
         return True
     return is_palindrome_recursive(word[1:-1], low_index, high_index - 2)
