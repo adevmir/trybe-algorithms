3759588ccd6cd26b8df7f8f102eea26a416ba814
adevmir <ademirvalonqar@gmail.com>
2023-03-22 18:03:44 -0300

    [Requisito 03] - Recebe uma STRING, avalia se ela e um palindromo e retorna true ou false

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0 or word[low_index] != word[high_index]:
+        return False
+    if len(word) == 1 or len(word) == 2 and word[low_index] == word[high_index]:
+        return True
+    return is_palindrome_recursive(word[1:-1], low_index, high_index - 2)
