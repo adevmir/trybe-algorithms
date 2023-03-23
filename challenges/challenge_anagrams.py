8cc71f4b4c7a1d2ef2986f17a8d9766dd89527c2
adevmir <ademirvalonqar@gmail.com>
2023-03-23 19:25:40 -0300

    [Requisito 4] - Funcao refatorada

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,29 +1,20 @@
+def sort_string(string):
+    for i in range(0, len(string)):
+        i_short = i
+        for j in range(i + 1, len(string)):
+            if string[j] < string[i_short]:
+                i_short = j
+        if i_short != i:
+            aux = string[i_short]
+            string[i_short] = string[i]
+            string[i] = aux
+    return (''.join(string))
+
+
 def is_anagram(first_string, second_string):
-    first_string = list(first_string.casefold())
-    second_string = list(second_string.casefold())
-    for i in range(0, len(first_string)):
-        i_short = i
-        for j in range(i + 1, len(first_string)):
-            if first_string[j] < first_string[i_short]:
-                i_short = j
-        if i_short != i:
-            aux = first_string[i_short]
-            first_string[i_short] = first_string[i]
-            first_string[i] = aux
-    for i in range(0, len(second_string)):
-        i_short = i
-        for j in range(i + 1, len(second_string)):
-            if second_string[j] < second_string[i_short]:
-                i_short = j
-        if i_short != i:
-            aux = second_string[i_short]
-            second_string[i_short] = second_string[i]
-            second_string[i] = aux
-            i_short = i
-    first_string = ''.join(first_string)
-    second_string = ''.join(second_string)
-    if first_string == '' or second_string == ''\
-            or len(first_string) != len(second_string):
-        return (first_string, second_string, False)
+    first_string = sort_string(list(first_string.casefold()))
+    second_string = sort_string(list(second_string.casefold()))
     if first_string == second_string:
         return (first_string, second_string, True)
+    else:
+        return (first_string, second_string, False)
